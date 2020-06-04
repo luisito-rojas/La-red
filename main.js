224208562747dcbed18346a37230c6851d5bb27d
Luis Rojas <luis.torres.rojas.18@gmail.com>
2020-06-04 15:22:24 -0500

    Este es mi primer commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click",() => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.banner-one', {delay: 500});
+ScrollReveal().reveal('.banner-two', {delay: 500});
+ScrollReveal().reveal('.footer-links', {delay: 500});
+ScrollReveal().reveal('.footer', {delay: 500});
